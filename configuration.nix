7a4324eec4fc10049e46149a8934db9db397013c
Simon Gergely <simon.gergely2006@gmail.com>
2026-09-08 22:33:33 +0200

    Populate configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -7,20 +7,62 @@
   ];
 
   # 2. Boot
-  # boot....
+# Boot
+boot.loader.systemd-boot.enable = true;
+boot.loader.efi.canTouchEfiVariables = true;
 
   # 3. Networking
-  # networking....
+# Networking
+networking.hostName = "nix";
+networking.networkmanager.enable = true;
 
   # 4. Localization
-  # time....
-  # i18n....
+time.timeZone = "Europe/Budapest";
+
+i18n.defaultLocale = "en_US.UTF-8";
+
+i18n.extraLocaleSettings = {
+  LC_ADDRESS = "hu_HU.UTF-8";
+  LC_IDENTIFICATION = "hu_HU.UTF-8";
+  LC_MEASUREMENT = "hu_HU.UTF-8";
+  LC_MONETARY = "hu_HU.UTF-8";
+  LC_NAME = "hu_HU.UTF-8";
+  LC_NUMERIC = "hu_HU.UTF-8";
+  LC_PAPER = "hu_HU.UTF-8";
+  LC_TELEPHONE = "hu_HU.UTF-8";
+  LC_TIME = "hu_HU.UTF-8";
+};
+
+services.xserver.xkb.layout = "hu";
+console.keyMap = "hu";
 
   # 5. Users
-  # users....
+# Users
+users.users.gergo = {
+  isNormalUser = true;
+
+  extraGroups = [
+    "wheel"
+    "networkmanager"
+  ];
+};
 
   # 6. Packages
-  # environment.systemPackages = ...
+environment.systemPackages = with pkgs; [
+  # Editors
+  vim
+
+  # Development / CLI
+  git
+  wget
+  curl
+
+  # System utilities
+  htop
+  tree
+  unzip
+  zip
+];
 
   # 7. Services
   # services....
@@ -29,7 +71,11 @@
   # programs....
 
   # 9. Nix configuration
-  # nix....
+# Nix
+nix.settings.experimental-features = [
+  "nix-command"
+  "flakes"
+];
 
   # 10. NixOS compatibility version
   system.stateVersion = "26.05";
